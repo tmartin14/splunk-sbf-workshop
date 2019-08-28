e7e1e7d0477aa14efc6dae7a4e78b48425c39e6c
Tom Martin <tmartin14@gmail.com>
2019-08-28 09:50:05 -0400

    Initial Commit

---
+++ run_sbf.sh
@@ -0,0 +1,28 @@
+docker run -t --name=splunk_business_flow -d -p 8000:8000 -p 8088:8088 \
+    -e "SPLUNK_START_ARGS=--accept-license" \
+    -e "SPLUNK_PASSWORD=changeme" 
+    -e "SPLUNK_APPS_URL=/tmp/splunk-business-flow_102.tgz" \
+tmartin14/splunk_business_flow:latest
+
+
+# Helpful Commands
+
+# inspect the logs
+# docker logs -f splunk_business_flow
+
+# log into the docker container
+# docker container exec -it --user root splunk_business_flow bash 
+
+# Stop and remove the container
+# docker stop splunk_business_flow && docker rm splunk_business_flow
+
+
+
+
+# Docker Compose options
+
+# if you prefer to use docker-compose, here's your command
+# docker-compose -f compose-sbf.yml up 
+
+# stop the docker-compose instance
+# docker-compose -f compose-sbf.yml down 
